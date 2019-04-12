88f4c94b4daf0fdedc0b24802692344b0e360e7b
frcharry <fabio.charry@gmail.com>
2019-04-11 20:51:04 -0500

    Sexto commit

--- solution.rb
+++ solution.rb
@@ -2,7 +2,9 @@ require 'sinatra'
 
 get '/' do
   unless params[:nombre]
-    "Hola Desconocido!"
+    <<-HTML
+      <h1>"Hola Desconocido!"</h1>
+    HTML
   else
     <<-HTML
       <h1>Hola #{params[:nombre]}!</h1>
